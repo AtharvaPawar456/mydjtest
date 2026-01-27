af08d752d972d060fd5cff713c121ee8b50a549b
Atharva Prashant Pawar <67649944+AtharvaPawar456@users.noreply.github.com>
2026-01-27 10:01:01 +0530

    fix-27Jan2026

--- handmadeprojects/urls.py
+++ handmadeprojects/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('hpmadmin/', admin.site.urls),
-    # path('', include('mainapp.urls'))
+    # path('hpmadmin/', admin.site.urls),
+    path('', include('mainapp.urls'))
 ]
